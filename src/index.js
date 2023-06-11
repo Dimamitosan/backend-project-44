aa774535ffb8efaa6ae91adf6ecadce9ba341a3d
Dmitrii Shagarov <shagarov07@gmail.com>
2023-06-11 13:58:28 +0300

    q

--- src/index.js
+++ src/index.js
@@ -10,17 +10,21 @@ export default (logic,start) => {
   console.log('Welcome to the Brain Games!');
   const name = askName();
   console.log(start)
-  for(let count = 0; count < 3; count ++) {
+  let count = 0;
+  while(count <3) {
     const [question,rightAnswer] = logic() 
     console.log(question)
     const yourAnswer = readlineSync.question('Your answer: ');
     if (rightAnswer === yourAnswer) {
       console.log('Correct');
+      count +=1
     } else {
       console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'`);
       console.log(`Let's try again ${name}`);
-      count = -1
+
     }  
+    if (count ===3){
+      console.log(`Congratulations, ${name}!`);
+    }
   }
-    console.log(`Congratulations, ${name}!`);
 }
