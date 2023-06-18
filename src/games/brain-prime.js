ca3f85fd0cd7d8ca635bbaddda3b47dd4d4a04a0
Dima <shagarov07@gmail.com>
2023-06-19 01:03:54 +0300

    rework prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -5,17 +5,20 @@ const start = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
   const numSqrt = number**(0.5) 
-  for (let i = 2; i <= numSqrt; i++) {
+  let count = 0;
+  for (let i = 1; i <= numSqrt; i++) {
     if (number % i === 0) {
-      return false
+      count+=2
     }
+  }
+  if (count ===2){
     return true
   }
+  return false
 }
 
 const prime = () => {
   const number = random(1, 10);
-  let flag = true;
   const question = `Question: ${number}`;
   const rightAnswer = isPrime(number) ? 'yes' : 'no';
   return [question, rightAnswer];
