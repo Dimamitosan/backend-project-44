e77058d247ed55fb3ccb7a3e6736596844f45db8
Dima <shagarov07@gmail.com>
2023-06-19 00:41:54 +0300

    rework progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -5,7 +5,7 @@ const start = 'What number is missing in the progression?';
 
 const genProg = (number,step,endOfProgression) => {
   const progression = [];
-  while (progression.length <= endOfProgression + 5) {
+  while (progression.length < endOfProgression + 5) {
     progression.push(number);
     number += step;
   }
@@ -14,7 +14,7 @@ const genProg = (number,step,endOfProgression) => {
 
 const prog = () => {
   const progression = genProg(random(),random(),random(1,5))
-  const possition = random(1, progression.length);
+  const possition = random(1, progression.length-1);
   const rightAnswer = String(progression[possition]);
   progression[possition] = '..';
   const question = `Question: ${progression.join(' ')}`;
